69047d233278b1332e8c95eac7a343fe4a35da49
ladleva <demihova-lera@rambler.ru>
2022-02-28 20:00:17 +0300

    New functions

--- Project1.js
+++ Project1.js
@@ -1,16 +1,47 @@
-let view = document.getElementById('read-more');
-let moreInfo = document.getElementById('more-info');
-let close = document.getElementById('close-button')
+function readMore() {
+    let dots = document.getElementById("dots");
+    let more = document.getElementById("more");
+    let btn = document.getElementById("btn");
 
-let open = function() {
-    moreInfo.style.display = 'block';
-    close.style.display = 'block';
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Открыть отзыв";
+        more.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Закрыть отзыв";
+        more.style.display = "inline";
+    }
 }
 
-let hide = function() {
-    moreInfo.style.display = 'none'
-    close.style.display = 'none';
+function readMore1() {
+    let dots1 = document.getElementById("dots1");
+    let more1 = document.getElementById("more1");
+    let btn1 = document.getElementById("btn1");
+
+    if (dots1.style.display === "none") {
+        dots1.style.display = "inline";
+        btn1.innerHTML = "Открыть отзыв";
+        more1.style.display = "none";
+    } else {
+        dots1.style.display = "none";
+        btn1.innerHTML = "Закрыть отзыв";
+        more1.style.display = "inline";
+    }
 }
 
-view.addEventListener('click', open);
-close.addEventListener('click', hide);
+function readMore2() {
+    let dots2 = document.getElementById("dots2");
+    let more2 = document.getElementById("more2");
+    let btn2 = document.getElementById("btn2");
+
+    if (dots2.style.display === "none") {
+        dots2.style.display = "inline";
+        btn2.innerHTML = "Открыть отзыв";
+        more2.style.display = "none";
+    } else {
+        dots2.style.display = "none";
+        btn2.innerHTML = "Закрыть отзыв";
+        more2.style.display = "inline";
+    }
+}
