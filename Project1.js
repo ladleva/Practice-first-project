2e086fd519bdaabaa3347db2ddc8e17b06dff3ab
ladleva <demihova-lera@rambler.ru>
2022-03-01 09:38:15 +0300

    Done

--- Project1.js
+++ Project1.js
@@ -1,47 +1,19 @@
-function readMore() {
-    let dots = document.getElementById("dots");
-    let more = document.getElementById("more");
-    let btn = document.getElementById("btn");
+let btns = document.querySelectorAll('.btn');
 
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btn.innerHTML = "Открыть отзыв";
-        more.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btn.innerHTML = "Закрыть отзыв";
-        more.style.display = "inline";
-    }
-}
+for (btn of btns) {
+    btn.addEventListener('click', function() {
+        let client = this.closest('.client');
+        let dots = client.querySelector('.dots');
+        let more = client.querySelector('.more');
 
-function readMore1() {
-    let dots1 = document.getElementById("dots1");
-    let more1 = document.getElementById("more1");
-    let btn1 = document.getElementById("btn1");
-
-    if (dots1.style.display === "none") {
-        dots1.style.display = "inline";
-        btn1.innerHTML = "Открыть отзыв";
-        more1.style.display = "none";
-    } else {
-        dots1.style.display = "none";
-        btn1.innerHTML = "Закрыть отзыв";
-        more1.style.display = "inline";
-    }
-}
-
-function readMore2() {
-    let dots2 = document.getElementById("dots2");
-    let more2 = document.getElementById("more2");
-    let btn2 = document.getElementById("btn2");
-
-    if (dots2.style.display === "none") {
-        dots2.style.display = "inline";
-        btn2.innerHTML = "Открыть отзыв";
-        more2.style.display = "none";
-    } else {
-        dots2.style.display = "none";
-        btn2.innerHTML = "Закрыть отзыв";
-        more2.style.display = "inline";
-    }
+        if (dots.style.display === "none") {
+            dots.style.display = "inline";
+            this.textContent = "Открыть отзыв"
+            more.style.display = "none";
+        } else {
+            dots.style.display = "none";
+            this.textContent = "Закрыть отзыв"
+            more.style.display = "inline";
+        }
+    });
 }
